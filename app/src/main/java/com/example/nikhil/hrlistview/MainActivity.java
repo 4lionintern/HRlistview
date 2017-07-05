19048b1af8eaaa51b2508269dddf4a7b2210ba39
nikhil-pandita <nikhil-pandita>
2017-07-05 16:23:58 +0530

    dynamic recylcer view

--- app/src/main/java/com/example/nikhil/hrlistview/MainActivity.java
+++ app/src/main/java/com/example/nikhil/hrlistview/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.nikhil.hrlistview;
 
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import org.lucasr.twowayview.TwoWayView;
 
 import java.util.ArrayList;
 
@@ -13,33 +19,54 @@ public class MainActivity extends AppCompatActivity  {
     private RecyclerView mRecyclerView;
     private HorizontalRecyclerViewAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    LinearLayout linearLayout;
+    NestedScrollView nestedScrollView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view);
+
+
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nsv);
+        linearLayout = (LinearLayout) findViewById(R.id.ll);
+        nestedScrollView.removeAllViews();
+        nestedScrollView.addView(linearLayout);
+
+
+
+        for (int i=1;i<=4;i++)
+        {
+            final RecyclerView xRecyclerview = new RecyclerView(this);
+            xRecyclerview.findViewById(R.id.fragment_horizontal_recycler_view+i);
+            xRecyclerview.setHasFixedSize(true);
+            mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
+            xRecyclerview.setLayoutManager(mLayoutManager);
+            xRecyclerview.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
+            xRecyclerview.setAdapter(mAdapter);
+            linearLayout.addView(xRecyclerview);
+        }      /*  mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
 
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);//new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);   //new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
 
         mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
         mRecyclerView.setAdapter(mAdapter);
-        RecyclerView.ItemDecoration itemDecoration =
-                new DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL);
         mRecyclerView.addItemDecoration(itemDecoration);
-        mAdapter.setOnItemClickListener(this);
-
+        *//*mAdapter.setOnItemClickListener(this);*//*
 
 
         mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view2);
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);//new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);   //new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
@@ -52,7 +79,7 @@ public class MainActivity extends AppCompatActivity  {
         mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view3);
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);//new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);   //new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
@@ -63,7 +90,7 @@ public class MainActivity extends AppCompatActivity  {
         mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view4);
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);//new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);   //new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
@@ -74,13 +101,14 @@ public class MainActivity extends AppCompatActivity  {
         mRecyclerView = (RecyclerView) findViewById(R.id.fragment_horizontal_recycler_view5);
         mRecyclerView.setHasFixedSize(true);
 
-        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);//new LinearLayoutManager(this);
+        mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);   //new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         mAdapter = new HorizontalRecyclerViewAdapter(getDataSet());
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.addItemDecoration(itemDecoration);
-        mAdapter.setOnItemClickListener(this);
+        mAdapter.setOnItemClickListener(this);*/
+
 
         // Code to Add an item with default animation
         //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
